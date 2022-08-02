6490308ec6fa90ca92d0a29622d9b44473e73825
GlauberBispo <glauber.bispo@gmail.com>
2022-08-02 19:47:27 -0300

    Entrega exercicio2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -13,3 +13,19 @@ const filme = {
 };
 
 // respostas aqui ⬇️
+
+//a) Qual o código para imprimir o nome do **primeiro** ator/atriz?
+
+console.log('Primeiro ator:',filme.elenco[0]);
+
+//b) Qual o código para imprimir o nome do **último** ator/atriz?
+
+console.log('Último ator/atriz:',filme.elenco[filme.elenco.length - 1]);
+
+//c) Qual o código para exibir o array com **todas** as transmissões de hoje?
+
+console.log('Todas as transmissões de hoje:', filme.transmissoesHoje);
+
+//d) Qual o código para exibir o **horário** de transmissão do filme no `Canal Brasil`?
+
+console.log('Horário de transmissão Canal Brasil:', filme.transmissoesHoje[1].horario);
