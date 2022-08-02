5c40068ccda1dc3e3859032527171886c73734fc
Leonardo <hgtleohgt@gmail.com>
2022-08-02 17:53:23 -0300

    ExercicioA11LeonardoHenriqueA

---
+++ exercicio1/script.js
@@ -0,0 +1,18 @@
+const sacolao = []
+const primeiraFruta = {
+    nome:"Melão",
+    preco: 4.19,
+    temDisponivel:true
+}
+const segundaFruta = {
+    nome:"Banana",
+    preco: 7,
+    temDisponivel:true
+}
+const terceiraFruta = {
+    nome:"Maça",
+    preco: 3,
+    temDisponivel:true
+}
+sacolao.push(primeiraFruta,segundaFruta,terceiraFruta)
+console.log(sacolao)
