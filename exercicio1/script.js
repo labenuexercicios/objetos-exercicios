b3bc6e9ce8dd2fe13ad442eb375e063b7a812dab
Marco_Borges <mas.borges78@gmail.com>
2022-08-02 21:25:21 -0300

    Exercício feitos

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+const sacolao = []
+
+console.log(sacolao)
+
+//Questão A
+const listaSacolao = [{
+    nome: 'Banana',
+    preco: 6.99,
+    disponibilidade: true,
+},{
+    nome: 'Limão',
+    preco: 4.99,
+    disponibilidade: true,
+},{
+    nome: 'Ovos',
+    preco: 12.99,
+    disponibilidade: false,
+}]
+
+//Questão B
+
+sacolao.push(listaSacolao)
+
+//Questão C
+console.log(sacolao)
+
