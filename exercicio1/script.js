359e42b8752a51dbb2330f41f1ddea1b87ee1976
=Emerson78costad <emersoncostadantas1995@gmail.com>
2022-08-07 20:37:40 -0400

    aula11-objetos completa

---
+++ exercicio1/script.js
@@ -0,0 +1,27 @@
+// Crie um array vazio chamado `sacolao`
+
+let sacolao = []
+
+// Crie três objetos que vão representar frutas do sacolão
+let fruta1 = {
+    nome: "maçã",
+    preco: "2:00",
+    disponibilidade: false
+}
+
+let fruta2 = {
+    nome: "abacaxi",
+    preco: "4:00",
+    disponibilidade: true
+}
+
+let fruta3 = {
+    nome: "banana",
+    preco: "5:00",
+    disponibilidade: true
+}
+/// Adicione os objetos ao array `sacolao` utilizando o método **`push()`**
+/* sacolao.push("carro") */
+sacolao.push( fruta1, fruta2, fruta3)
+console.log(sacolao)
+ 
