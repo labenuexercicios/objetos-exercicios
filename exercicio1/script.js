9c584c43e140d4458a1cf89d047609aa775ffe32
PYTERODACTYL <pyter512@live.com>
2022-09-05 10:37:29 -0300

    feito

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+let sacolao = []
+
+let fruta1 = {nome: "Jabuticaba",preco: 1.99, disponibilidade: true}
+let fruta2 = {nome: "Carambola", preco: 5.99, disponibilidade: false}
+let fruta3 = {nome: "Abacaxi", preco: 4.99, disponibilidade: true}
+
+sacolao.push(fruta1)
+sacolao.push(fruta2)
+sacolao.push(fruta3)
+
+console.log(sacolao)
