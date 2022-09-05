7d0f775281b3b077ca3add6e550ece91b383a389
MarianaMeijon <marianameijon1989@gmail.com>
2022-09-05 16:21:08 -0300

    Exercicios 1,2 e 3 finalizados

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -13,3 +13,23 @@ const filme = {
 };
 
 // respostas aqui ⬇️
+
+//a) Qual o código para imprimir o nome do **primeiro** ator/atriz?
+
+const qualAtor = filme.elenco.slice(0,1)
+console.log (qualAtor)
+
+// b) Qual o código para imprimir o nome do **último** ator/atriz?
+
+const ultimoAtor = filme.elenco.slice(length-1)
+console.log (ultimoAtor)
+
+//c) Qual o código para exibir o array com **todas** as transmissões de hoje?
+
+console.log (filme.transmissoesHoje)
+
+//d) Qual o código para exibir o **horário** de transmissão do filme no `Canal Brasil`?
+
+const horarioCanalbrasil = filme.transmissoesHoje[1].horario
+console.log (` O horário de transmissão do filme no Canal Brasil é ${horarioCanalbrasil}`)
+
