904deb0a4fc5c8260eec4a67f64252107d91af3e
Alex <alexferreiraa20@gmail.com>
2022-09-05 15:15:41 -0300

    Exercícios resolvidos - Alex Ferreira - Ammal-A

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -13,3 +13,17 @@ const filme = {
 };
 
 // respostas aqui ⬇️
+
+//resposta letra A: Qual o código para imprimir o nome do **primeiro** ator/atriz?
+console.log(`Primeiro ator/atriz: ${filme.elenco[0]}`)
+
+//resposta letra B: Qual o código para imprimir o nome do **último** ator/atriz?
+console.log(`último ator/atriz: ${filme.elenco[3]}`)
+
+//resposta letra C: Qual o código para exibir o array com **todas** as transmissões de hoje?
+console.log(`Essas são todas as transmissões de hoje: ${filme.transmissoesHoje}`)
+
+//resposta letra D:
+
+console.log(`Esse é o horário do "Canal Brasil": ${filme.transmissoesHoje[1].horario}`)
+
