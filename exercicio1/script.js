a9752f5f4ca87d43d3b4eef1612527f39f790584
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-09-05 10:58:58 -0300

    exercicio 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,23 @@
+const sacolao=[]
+
+const banana={
+    nome:'banana',
+    preco: 4.50,
+    disponibilidade:true,
+}
+const maca={
+    nome:'banana',
+    preco: 3,
+    disponibilidade:false
+}
+const mamao={
+    nome:'mamao',
+    preco:10,
+    disponibilidade:true
+}
+
+// sacolao.push(banana)
+// sacolao.push(mamao)
+// sacolao.push(maca)
+sacolao.push(maca, mamao, banana)
+console.log(sacolao)
