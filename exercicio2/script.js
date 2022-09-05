2ac54814799187df40989bed3dc6093c666628bb
pkamibayashi <paulo_Kamibayashi@hotmail.com>
2022-09-05 13:48:39 -0300

    Exercico2 Done

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -4,12 +4,23 @@ const filme = {
 	elenco: [
 		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
 		"Virginia Cavendish"
-		], 
+	], 
 	transmissoesHoje: [
 		{canal: "Telecine", horario: "21h"}, 
 		{canal: "Canal Brasil", horario: "19h"}, 
 		{canal: "Globo", horario: "14h"}
-		]
+	]
 };
 
 // respostas aqui ⬇️
+
+// a) Qual o código para imprimir o nome do **primeiro** ator/atriz?
+console.log(filme.elenco[0])
+
+// b) Qual o código para imprimir o nome do **último** ator/atriz?
+console.log(filme.elenco[3])
+// c) Qual o código para exibir o array com **todas** as transmissões de hoje?
+console.log(filme.transmissoesHoje)
+
+// d) Qual o código para exibir o **horário** de transmissão do filme no `Canal Brasil`?
+console.log(filme.transmissoesHoje[1].horario)
