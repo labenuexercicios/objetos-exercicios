05c6a1499bd6f0c88853bacaf5b20caf2883e058
Viniciusixx <vinitotheworld@gmail.com>
2022-09-05 15:14:08 -0300

    Mudança 2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,26 +4,22 @@
 const sacolao=[]
 console.log('==========')
 
-const ex10=[ 
      
-    {
+    const ex10= {
     nome: 'Laranja',
     preco: 1,
-    disponibilidade: sacolao === 3
-},
-    { 
+    disponibilidade: true}
+    
+    const ex11 = { 
      nome: 'uva',
      preço: 5,
-     disponibilidade: sacolao === sacolao  
-    },
-    { 
+     disponibilidade: false}
+     const ex12 = { 
     nome: 'morango',
     preço: 20,
-    disponibilidade: sacolao === []
-    }
-]
+    disponibilidade: false}
 // b) Adicione os objetos ao array `sacolao` utilizando o método **`push()`**
-const addSacolao = sacolao.push(ex10)
+const addSacolao = sacolao.push(ex10, ex11, ex12)
 
 // c) Imprima a o array `sacolao`, e certifique-se de que agora ela seja um array **com três objetos**
 console.log('Impressão do sacolao:',sacolao)
