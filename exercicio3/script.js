ada31e10ea69990322780b329bb2b7340921c068
euleo11 <leonardov.silva15@gmail.com>
2022-09-15 19:32:05 -0300

    esqueci de fazer o push desse exercicio

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -4,37 +4,20 @@ const pokemon1 = {
 	nivel: 5
 };
 
-const pokemon2 = {...pokemon1};
-pokemon2.nome = "Squirtle";
-pokemon2.tipo = "Água";
+const pokemon2 = {...pokemon1, nome: "Squirtle", tipo: "Água",};
 
-pokemon1.ataques = [];
-pokemon1.ataques. push({
-    nome:"Investida",
-    dano: 40,
-    tipo: "Normal",
-    precisao: 100,
-})
-// console.log(pokemon1)
+console.log(pokemon1)
+console.log(pokemon2)
 
-pokemon2.ataques = pokemon1.ataques.slice()
-// console.log(pokemon2)
-pokemon1.ataques.push({
-nome:"Folha Navalha",
-dano: 45,
-tipo: "Grama",
-precisao: 100,
-})
-// console.log(pokemon1)
+pokemon1.ataques = []
+console.log(pokemon1.ataques)
 
-pokemon2.ataques.push({
-nome: "Jato de Água",
-dano:40,
-tipo:"Água",
-precisao: 100,
-})
+const ataque1 = {nome: "Investida",
+dano: 40,
+tipo: "Normal",
+precisao: 100}
 
-console.log("POKEDEX:",
-    pokemon1,
-    pokemon2
-    )
+pokemon1.ataques.push(ataque1)
+console.log(pokemon1.ataques)
+
+console.log(pokemon2)
