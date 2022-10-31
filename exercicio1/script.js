cd4f7df546b224f7a4bc505c25431e0b203dbc9e
Anderson-Schi <jr93@hotmai.com>
2022-10-31 21:42:16 +0000

    Exercicio 1 resolvido

---
+++ exercicio1/script.js
@@ -0,0 +1,23 @@
+const sacolao = [];
+
+const objeto1 = {
+    nome: "banana",
+    preco: 2.5,
+    disponibilidade: true
+};
+
+const objeto2 = {
+    nome: "abacate",
+    preco: 0.75,
+    disponibilidade: true
+};
+
+const objeto3 = {
+    nome: "mamao",
+    preco: 1.75,
+    disponibilidade: false
+};
+
+sacolao.push(objeto1, objeto2, objeto3);
+
+console.log(sacolao);
