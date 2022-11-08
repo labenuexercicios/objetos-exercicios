6c07654aefbf0cbc0b97ef58fcc56a79b01a5c47
Alex Fernandes da Silva <alex.rgracie@gmail.com>
2022-11-07 23:09:26 -0300

    Exercicio Respondido

---
+++ exercicio1/script.js
@@ -0,0 +1,23 @@
+const sacolao = []
+
+const frutas = {
+    nome: "banana",
+    preco: 6.00,
+    disponibilidade: true
+}
+const frutas1 = {
+    nome: "maçã",
+    preco: 2.50,
+    disponibilidade: true
+}
+const frutas2 = {
+    nome: "kiwi",
+    preco: 3.50,
+    disponibilidade: false
+}
+
+sacolao.push(frutas)
+sacolao.push(frutas1)
+sacolao.push(frutas2)
+
+console.log(sacolao)
