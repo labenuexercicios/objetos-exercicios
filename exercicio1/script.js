3f7d46e4a323278220254da23fff55892b191fd7
USUÁRIO <sidasantos6395@gmail.com>
2022-12-04 14:59:37 -0300

    exercicio-objetos

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+const sacolao = []
+
+const frutas1 = { 
+    nome: 'mamao',
+    preco: 2.50,
+    disponibilidade: true
+
+
+
+}
+const frutas2 = {
+    nome: 'laranja',
+    preco: 4.0,
+    disponibilidade: false
+
+}
+const frutas3 = {
+    nome: 'melancia',
+    preco: 6.0,
+    disponibilidade: true
+
+}
+sacolao.push(frutas1)
+sacolao.push(frutas2)
+sacolao.push(frutas3)
+ console.log(sacolao)
