e7bf017b05f6c45b1d859a7da213de4f9d9aac66
LivanDoVale <livanjr4@gmail.com>
2023-02-15 14:38:53 -0300

    exercicio 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+const sacolao = []
+
+//A)
+const fruta1 = {
+    nome: "Maça",
+    preco: (0.50).toFixed(2),
+    disponibilidade: true
+}
+const fruta2 = {
+    nome: "Banana",
+    preco: (6).toFixed(2),
+    disponibilidade: true
+}
+const fruta3 = {
+    nome: "Melancia",
+    preco: (10).toFixed(2),
+    disponibilidade: false
+}
+
+//B)
+sacolao.push(fruta1, fruta2, fruta3)
+
+//C)
+console.log(sacolao)
