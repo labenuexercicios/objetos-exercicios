4a22de1fe00e320c0430667fc00e8e7ca48494bb
BenjaminMMRuiz <benjaminmartinhomoyaruiz@gmail.com>
2023-05-19 15:12:23 +0100

    Exercicios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,20 @@
+//# Exercício 2
+
+//Leia o código abaixo, e responda:
+
+//a) Qual o código para imprimir o nome do **primeiro** ator/atriz?
+
+//b) Qual o código para imprimir o nome do **último** ator/atriz?
+
+//c) Qual o código para exibir o array com **todas** as transmissões de hoje?
+
+//d) Qual o código para exibir o **horário** de transmissão do filme no **Canal Brasil**?
+
+
+//**Dica:** construa os caminhos para chegar em cada valor **aos poucos**, sempre verificando o que é exibido no console.
+
+
+
 const filme = {
 	nome: "Auto da Compadecida", 
 	ano: 2000, 
@@ -13,3 +30,8 @@ const filme = {
 }
 
 // respostas aqui ⬇️
+
+console.log(filme.elenco[0])
+console.log(filme.elenco[3])
+console.log(filme.transmissoesHoje)
+console.log(filme.transmissoesHoje[1])
