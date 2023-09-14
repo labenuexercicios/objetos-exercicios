2b0e8883f856bd5fc6a3232e4b04cd3115d77bbc
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-14 11:27:52 -0500

    exercicio1-feito

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+const sacolao = [
+
+]
+
+const fruta1 = {
+    nome: "Laranja",
+    preco: 6,
+    disponibilidade: true
+}
+
+const fruta2 = {
+    nome: "Banana",
+    preco: 7,
+    disponibilidade: false
+}
+
+const fruta3 = {
+    nome: "Kiwi",
+    preco: 9,
+    disponibilidade: false
+}
+
+sacolao.push({fruta1},{fruta2}, {fruta3}),
+
+
+console.log(sacolao);
