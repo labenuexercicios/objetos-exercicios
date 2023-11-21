6dd0c10220141e7e4ba51f4624a0c978a521ec06
bruhoffman <bruhoffman@gmail.com>
2023-11-21 18:48:47 -0300

    [Exercícios de Fixação] Exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,22 @@
+const sacolao = [];
+
+const fruta1 = {
+    nome: 'Maça',
+    preco: 2.5,
+    disponibilidade: true
+};
+
+const fruta2 = {
+    nome: 'Pera',
+    preco: 4,
+    disponibilidade: false
+};
+
+const fruta3 = {
+    nome: 'Laranja',
+    preco: 5,
+    disponibilidade: true
+};
+
+sacolao.push(fruta1, fruta2, fruta3);
+console.log(sacolao);
